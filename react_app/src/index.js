f640390863cc87f9fea626d5538816622daeb486
liuhao <liuhao@zgtozed.com>
2020-08-13 11:55:23 +0800

    更新

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'antd/lib/style/themes/default.less';
-import 'antd/dist/antd.less'; // 引入官方提供的 less 样式入口文件
+import 'antd/dist/antd.css'; // 引入官方提供的 less 样式入口文件
 import './assets/theme/theme.less'; // 用于覆盖上面定义的变量
 
 ReactDOM.render(<App />,document.getElementById('root'));
